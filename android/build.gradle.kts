1d2dae57d298ef71de92ec68fccfa003fb2a5f46
Aashish Verma <aashishverma042@gmail.com>
2026-04-24 13:03:55 +0530

    Add files via upload

---
+++ android/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    // Declare Google Services plugin but don't apply it here
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
